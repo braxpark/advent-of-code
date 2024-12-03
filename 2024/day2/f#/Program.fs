a7bc0d5239ee9010e56b48688e6dc47dc2697b58
Braxton Parker <78038984+braxpark@users.noreply.github.com>
2024-12-02 22:56:59 -0600

    add day2 part 1 f#

---
+++ 2024/day2/f#/Program.fs
@@ -0,0 +1,28 @@
+﻿open System.IO
+
+let part1 (filePath: string) (delimiter: string) =
+    File.ReadLines(filePath)
+    |> Seq.sumBy (fun report ->
+        let levels = report.Split(delimiter) |> Array.map int
+        let increasing = levels.[1] > levels.[0]
+
+        levels
+        |> Array.indexed
+        |> Array.skip 1
+        |> Array.forall (fun (i, _) ->
+            let isMonotonic =
+                if increasing then
+                    levels.[i] > levels.[i - 1]
+                else
+                    levels.[i] < levels.[i - 1]
+
+            let isSafeDiff =
+                let diff = abs (levels.[i] - levels.[i - 1]) in diff >= 1 && diff <= 3
+
+            isMonotonic && isSafeDiff)
+        |> fun isSafe -> if isSafe then 1 else 0)
+
+let testPath = "../test.txt"
+let inputPath = "../input.txt"
+let delimiter = " "
+printfn "Part 1 Answer: %d" (part1 inputPath delimiter)
